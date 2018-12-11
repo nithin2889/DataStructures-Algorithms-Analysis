59d2bfbc4f5cbd0039bf1b96e17d58d5250e7194
Nithin Prasad <nithin2889@gmail.com>
2018-12-11 19:46:46 +0530

    Bubble Sorting in Java.

---
+++ DSAUdemy/src/com/algo/sort/bubble/BubbleSorting.java
@@ -0,0 +1,36 @@
+package com.algo.sort.bubble;
+
+public class BubbleSorting {
+
+	public static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		int temp = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (arr[j - 1] > arr[j]) {
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 7, 4, 2, 3, 8, 5 };
+		
+		System.out.println("Before Sort");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "\t");
+		}
+		
+		BubbleSorting.bubbleSort(arr);
+		
+		System.out.println("\nAfter Sort");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "\t");
+		}
+	}
+
+}
